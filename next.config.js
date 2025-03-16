c20a4b4d26405e4aa376adecec5c9996c7fbc9a2
Amogh H <amogh.harsha2501@gmail.com>
2025-03-16 13:30:16 +0530

    Temporarily commit out folder for gh-pages

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/timerx-website',        // So that paths become /timerx-website/...
-  assetPrefix: '/timerx-website/',    // Ensures _next/static/ and other assets load from /timerx-website/
-
-};
-
-module.exports = nextConfig;
--- pages/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white text-center p-4 mt-12">
-      <p>© {new Date().getFullYear()} TimerX. All rights reserved.</p>
-    </footer>
-  );
-}
